b1fdca9d01835f0f2bde40e91a5b1b1dabaa5fec
Lujain Z <55849375+Lujain993@users.noreply.github.com>
2021-01-25 15:34:22 +0300

    hi

---
+++ ADHEED/helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ADHEED
+//
+//  Created by Lujain Z on 24/01/2021.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
