5ff153296f49ebd7f3bbebea1ef1e060c57d41bf
rayakhalid <reyakh.h@gmail.com>
2021-01-25 15:34:29 +0300

    test

---
+++ ADHEED/View Controllers/sViewController.swift
@@ -0,0 +1,29 @@
+//
+//  sViewController.swift
+//  ADHEED
+//
+//  Created by Raya Khalid on 25/01/2021.
+//
+
+import UIKit
+
+class sViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+// raya
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
